ca7412ead5aa65632fe8a1071b66b6dfc96db5dd
Andre Blankholm <ablankholm@gmail.com>
2026-02-25 17:52:35 -0600

    Intial commit, added service container and Roll maganager

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RoleClaimsApp.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add service container
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+RoleManager<IdentityRole> roleManager = app.Services.GetRequiredService<RoleManager<IdentityRole>>();
+
+if (!await roleManager.RoleExistsAsync("Admin"))
+{
+    await roleManager.CreateAsync(new IdentityRole("Admin"));
+}
+if (!await roleManager.RoleExistsAsync("User"))
+{
+    await roleManager.CreateAsync(new IdentityRole("User"));
+}
+
+Claim claim = new Claim("Permission", "ManageEmployeeRecords");
+
+
+// Map controllers
+app.MapControllers();
+
+app.Run();
